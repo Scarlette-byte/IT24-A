ee8f438679250f3b68bf4f66cbc104f010500cac
heytonixx <dongkiecanete0@gmail.com>
2024-10-11 11:39:36 +0800

    added updates for dropdown

--- applet-4.js
+++ applet-4.js
@@ -11,46 +11,43 @@ class StudentList {
         this.renderStudentList(this.students); 
         this.bindSearchEvent();
     }
-}
+
     async fetchData() {
-    try {
-        const response = await fetch(this.dataUrl);
-        this.students = await response.json();
-    } catch (error) {
-        console.error('Error fetching data:', error);
+        try {
+            const response = await fetch(this.dataUrl);
+            this.students = await response.json();
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    }
+
+    renderStudentList(students, container = document.getElementById('studentList')) {
+        container.innerHTML = students.map(student => 
+            `<button class="btn btn-primary" style="margin-top:15px; 
+                                                    width:25rem">
+                ${student.student_name} | ${student.student_program}
+            </button><br>`
+        ).join('');
+    }
+
+    bindSearchEvent() {
+        const studentSearchBar = document.getElementById('studentSearchBar');
+        const studentSearchListContainer = document.getElementById('studentSearchList');
+
+        studentSearchBar.addEventListener('input', () => {
+            this.filterStudents(studentSearchBar.value, studentSearchListContainer);
+        });
+    }
+
+    filterStudents(query, searchListContainer) {
+        const filteredStudents = this.students.filter(student => {
+            const fullName = `${student.student_name} ${student.student_program}`;
+            return fullName.toLowerCase().includes(query.toLowerCase());
+        });
+
+        this.renderStudentList(filteredStudents, searchListContainer);
+     
     }
 }
-    renderStudentList(students) {
-    const studentListContainer = document.getElementById('studentList');
-    studentListContainer.innerHTML = students.map(student => 
-        `<button class="btn btn-primary" style="margin-top:15px; 
-                                                width:25rem">
-            ${student.student_name} | ${student.student_program}
-        </button><br>`
-    ).join('');
-}
-    bindSearchEvent() {
-    const studentSearchBar = document.getElementById('studentSearchBar');
-    const studentSearchListContainer = document.getElementById('studentSearchList');
-
-    studentSearchBar.addEventListener('input', () => {
-        this.filterStudents(studentSearchBar.value, studentSearchListContainer);
-    });
-
-    this.renderStudentList(this.students, studentSearchListContainer);
-}
-filterStudents(query, searchListContainer) {
-    const filteredStudents = this.students.filter(student => {
-        const fullName = `${student.student_name} ${student.student_program}`;
-        return fullName.toLowerCase().includes(query.toLowerCase());
-    });
-
-    searchListContainer.innerHTML = '';
-
-    this.renderStudentList(filteredStudents, searchListContainer);
-     
-}
-
-
 
 const studentList = new StudentList('applet-4.json');
