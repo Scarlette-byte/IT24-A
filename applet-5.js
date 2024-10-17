01667feaa50f5ac3e5aaee562c2a5cce28de8e88
Scarlette-byte <mancawanscarlette36@gmail.com>
2024-10-17 18:13:30 +0800

    added async fetch weather

--- applet-5.js
+++ applet-5.js
@@ -32,3 +32,44 @@ class WeatherService extends WeatherApp {
         super();
         this.apiKey = '';
     }
+    async fetchWeather() {
+        const city = this.cityInput.value;
+        this.apiKey = this.APIKeyInput.value.trim(); 
+
+        if (!this.apiKey) {
+            alert('Please enter your API key.');
+            return;
+        }
+
+        if (city) {
+            const data = await this.getWeatherData(city);
+            if (data) {
+                this.displayWeather(data);
+            } else {
+                alert('City not found. Please try again.');
+            }
+        } else {
+            alert('Please enter a city name.');
+        }
+    }
+    async fetchWeatherByLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                async (position) => {
+                    const { latitude, longitude } = position.coords;
+                    const data = await this.getWeatherDataByCoordinates(latitude, longitude);
+                    if (data) {
+                        this.displayWeather(data);
+                        this.cityInput.value = '';
+                    } else {
+                        alert('Unable to retrieve weather data for your location.');
+                    }
+                },
+                () => {
+                    alert('Unable to retrieve your location. Please allow location access.');
+                }
+            );
+        } else {
+            alert('Geolocation is not supported by this browser.');
+        }
+    }
